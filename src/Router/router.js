959964a22c8fb1b9af0ff5843a0a523142f4c6c4
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-28 16:42:53 +0100

    Main page master added

--- src/Router/router.js
+++ src/Router/router.js
@@ -3,16 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import {Signup} from "../pages/Signup";
 import {Signin} from "../pages/Signin";
+import {Main} from "../pages/Main";
 import {Navbar} from "../components/Navbar";
+import {StickyFooter} from "../components/Footer";
+
 
 function AppRouter() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/register" component={Signup} />
-        <Route path="/login" component={Signin} />
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/login" component={Signin} />
+        <Route path="/" component={Main} />
       </Switch>
+      <StickyFooter />
     </Router>
   );
 }
